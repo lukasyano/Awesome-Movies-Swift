964fce5d8dbfcbeb5a9c9d91297fe247e432b73e
lukasyano <toliusislukas@yahoo.com>
2024-05-08 08:56:46 +0300

    Initial Commit

---
+++ Awesome Movies/Awesome_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome_MoviesApp.swift
+//  Awesome Movies
+//
+//  Created by Lukas Toliusis on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Awesome_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Awesome Movies
+//
+//  Created by Lukas Toliusis on 08/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
